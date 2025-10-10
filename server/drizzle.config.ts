905bcf8b67dc41b58e6fcdb114ca9b9a02035fd5
Anish Sahoo <sahoo.an@northeastern.edu>
2025-10-10 10:32:32 -0400

    Add drizzle db credentials and add dotenv to othe drizzle scripts (#23)

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -4,4 +4,15 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/data/db/schema",
   out: "./src/data/db/migrations",
+  dbCredentials: {
+    host: process.env.POSTGRES_HOST ?? "localhost",
+    port: Number(process.env.POSTGRES_PORT ?? 5432),
+    database: process.env.POSTGRES_DB ?? "comm_ng",
+    user: process.env.POSTGRES_USER ?? "postgres",
+    password: process.env.POSTGRES_PASSWORD ?? "",
+    ssl:
+      process.env.POSTGRES_SSL === "true"
+        ? { rejectUnauthorized: false }
+        : false,
+  },
 });
