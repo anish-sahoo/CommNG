ebb87f6fd1d497b5009f02855edaedf67c7f08ae
Caroline Pham <carolinetpham@outlook.com>
2025-10-22 19:50:41 -0400

    feat(web): Creating communication channel navigation bar (#42)

--- web/src/components/navigation/index.tsx
+++ web/src/components/navigation/index.tsx
@@ -1,9 +1,11 @@
 import AppNavBar from "./app-navbar";
+import CommsNavBar from "./comms-navbar";
 
 const Navigation = () => {
   return (
     <div id="navigation">
       <AppNavBar />
+      <CommsNavBar />
     </div>
   );
 };
