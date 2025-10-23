c1bcd042a42d40dca2b516804ef91df3a4e54ed5
natashasun <sun.nata@northeastern.edu>
2025-10-23 10:29:15 -0400

    Add button feature

--- web/src/app/components/page.tsx
+++ web/src/app/components/page.tsx
@@ -1,9 +1,13 @@
+import { SelectableButton } from "@/components/buttons";
 import Navigation from "@/components/navigation";
 
 const Components = () => {
   return (
     <div id="components">
       <Navigation />
+      <div className="flex flex-1 items-center justify-center h-screen">
+        <SelectableButton text="New Post" icon="add" />
+      </div>
     </div>
   );
 };
