768c580dc7dadcd69d74e4ae2b7b0fb26363f8f6
Thomas Young <thomasjonyoung@gmail.com>
2025-11-29 15:21:25 -0500

    TRPC inference type helper

--- web/src/lib/trpc.ts
+++ web/src/lib/trpc.ts
@@ -5,3 +5,10 @@ import { createTRPCContext } from "@trpc/tanstack-react-query";
 
 export const { TRPCProvider, useTRPC, useTRPCClient } =
   createTRPCContext<AppRouter>();
+
+export type TRPCProcedures = ReturnType<typeof useTRPCClient>;
+
+// Utility type to extract the return type of a tRPC procedure
+export type InferTRPCOutput<T> = T extends { query: (...args: any) => any }
+  ? Awaited<ReturnType<T["query"]>>
+  : never;
